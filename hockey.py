1ef553558525b45c518a3cb13f97d902ec5793c8
SurfaceNote <surfacenote@gmail.com>
2018-11-17 14:07:35 +0700

    Update hockey.py

--- hockey.py
+++ hockey.py
@@ -7,20 +7,26 @@ from os.path import isfile, join
 
 def main():
     
-    start = input("'Начать' или 'завершить'?")
-    start.lower()
+    start = input("Введи 'начать' или 'завершить'? ").lower()
+
+    if start != "начать" and start != "завершить":
+        print("Введи слово правильно, пожалуйста.\n")
+        print(start)
+        main()
 
     if start == 'начать':
-        link_folder = input('Укажи директорию файлов матча (Например: "E:/Работа/хоккей") ')
+        link_folder = input('Укажи директорию файлов матча (Например: "E:/Работа/хоккей/Irbis - Reactor") ')
         link_folder = link_folder.replace("\\", '/')
-        a = input("Сколько у тебя всего партов?")
-        b = input("С какого парта?")
+        a = input("Сколько у тебя всего партов? (введи число): ")
+        b = input("С какого парта? (введи число): ")
 
 
         for new_folder in range(int(a)):
             name = "part-" + str(b)
             os.chdir(link_folder)
             os.mkdir(name)
+            os.chdir(link_folder + "/" + name)
+            os.mkdir("final")
             b = int(b) + 1
 
     if start == 'завершить':
@@ -36,13 +42,14 @@ def main():
         name = "part-"
         club1 = input("Домашняя команда: ")
         club2 = input("Гостевая команда: ")
-        a = input("Сколько у тебя всего партов?")
-        b = input("С какого парта?")
+        a = input("Сколько у тебя всего партов? (введи число): ")
+        b = input("С какого парта? (введи число): ")
 
         for n in range(int(a)):
             os.chdir(link_folder + "/result")
             final_name = name + str(b)
             zip_name1 = final_name + " " + club1 + " - " + club2 + ".zip"
+            zip_name2 = final_name + " " + club1 + " - " + club2
             shutil.copytree(link_folder + '/' + str(final_name) + '/final/', str(final_name))
             shutil.copytree(link_folder + '/' + str(final_name) + '/embedded/index', str(final_name) + '/index')
 
@@ -62,6 +69,8 @@ def main():
             with zipfile.ZipFile(zip_name1, 'a') as myzip:
                 myzip.write(csv_file[0])
                 myzip.write('index/autosave.csv')
+
+            shutil.move(link_folder + "/result/" + final_name + "/" + str(zip_name1), link_folder + "/result/")
 
             b = int(b) + 1
 
